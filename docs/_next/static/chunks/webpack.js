087093c47d8306858631b7cb55e4335d97de0217
Jonne Kaunisto <jonneka@gmail.com>
2021-11-05 17:28:51 -0700

    Updated docs

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b977681d89954d1a9af"; }
+/******/ 		__webpack_require__.h = function() { return "a973019bf76d06d855e2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
