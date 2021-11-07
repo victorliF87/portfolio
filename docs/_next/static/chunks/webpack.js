a3b0e849acba3fdd9f44a4626aa3cad1fb9c3161
Victor Li <victorli428@gmail.com>
2021-11-07 15:39:26 -0800

    bugfix

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd1dfda1374b484797f1"; }
+/******/ 		__webpack_require__.h = function() { return "a363c93c176074ee7ad4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
